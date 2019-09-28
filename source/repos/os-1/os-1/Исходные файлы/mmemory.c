8677de3f42bd088101409a0617b04670b05b1093
Yatsenko Yaroslav <yarikyatsenko@mail.ru>
2019-09-28 12:59:15 +0300

    write read funct

--- source/repos/os-1/os-1/Исходные файлы/mmemory.c
+++ source/repos/os-1/os-1/Исходные файлы/mmemory.c
@@ -40,9 +40,32 @@ int _free(VA ptr) {
 	if (ptr == NULL) return -1;
 	Node *iter_node = memory_storage->head;
 	while (iter_node != NULL) {
+		if (iter_node->value->va == ptr) {
+			if ((iter_node->prev != NULL) && (iter_node->prev->value->empty)) {
+				iter_node->prev->value->size += iter_node->value->size;
+				iter_node = iter_node->prev;
+				delete_node(memory_storage, iter_node);
+				return 0;
+			}
+			if ((iter_node->next != NULL)&&(iter_node->next->value->empty)) {
+				iter_node->value->size += iter_node->next->value->size;
+				delete_node(memory_storage, iter_node->next);
+				return 0;
+			}
+		}
+		if (iter_node->next == NULL) return 1;
+		iter_node = iter_node->next;
+	}
+	return 1;
+}
+
+
+int _read(VA ptr, VA pBuffer, t_size szBuffer) {
+	if ((ptr == NULL)||(pBuffer == NULL)||(memory_storage == NULL)) return -1;
+	Node *iter_node = memory_storage->head;
+	while (iter_node != NULL) {
 		if ((iter_node->value->va == ptr) && (!iter_node->value->empty)) {
-			// ifs and deleting
-			return 0;
+			
 		}
 		if (iter_node->next == NULL) return 1;
 		iter_node = iter_node->next;
