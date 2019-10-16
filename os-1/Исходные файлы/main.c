d05cee235b51bac61b824bb0237e6250c0917c33
Yatsenko Yaroslav <yarikyatsenko@mail.ru>
2019-10-16 23:29:54 +0300

    add wrong tests

--- os-1/Исходные файлы/main.c
+++ os-1/Исходные файлы/main.c
@@ -1,19 +1,177 @@
 #include <stdio.h>
+#include <assert.h>
 #include "../Файлы заголовков/mmemory.h"
 
 
-int main() {
-	VA ptr1, ptr2, ptr3;
-	void* bf = NULL;
-	size_t bfsize = 5;
+void wrapper(int code, int waiting_code) {
+	if (code == 0) puts("- Success work");
+	else if (code == 1) puts("- Unknown error");
+	else if (code == -1) puts("- Wrong parameters");
+	else if (code == -2) puts("- Memory leak");
+	assert(code == waiting_code);
+}
+
+
+void init_case0() {
 	int page_number = 3;
-	size_t page_size = 4;
+	size_t page_size = 10;
+	puts("Success initialization");
+	wrapper(_init(page_number, page_size), 0);
+}
+
+void init_case_1() {
+	int page_number = -3;
+	size_t page_size = -10;
+	puts("Initialization with wrong parameters");
+	wrapper(_init(page_number, page_size), -1);
+}
+
+void malloc_case0() {
+	int page_number = 3;
+	size_t page_size = 10;
+	VA ptr;
+	size_t bfsize = 5;
+	puts("Success malloc");
+	_init(page_number, page_size);
+	wrapper(_malloc(&ptr, bfsize), 0);
+}
+
+void malloc_case_1() {
+	int page_number = 3;
+	size_t page_size = 10;
+	VA ptr = NULL;
+	size_t bfsize = 5;
+	puts("Malloc with wrong parameters");
+	_init(page_number, page_size);
+	wrapper(_malloc(&ptr, bfsize), -1);
+}
+
+void malloc_case_2() {
+	int page_number = 3;
+	size_t page_size = 10;
+	VA ptr;
+	size_t bfsize = 31;
+	puts("Malloc with memory leak");
+	_init(page_number, page_size);
+	wrapper(_malloc(&ptr, bfsize), -2);
+}
+
+void read_case0() {
+	int page_number = 3;
+	size_t page_size = 10;
+	VA ptr;
+	char* buf_ptr_write = "aysd";
+	char* buf_ptr_read = (char*)malloc(sizeof(buf_ptr_write));
+	size_t bfsize = 5;
+	puts("Success read");
+	_init(page_number, page_size);
+	_malloc(&ptr, bfsize);
+	_write(ptr + bfsize, buf_ptr_write, sizeof(buf_ptr_write));
+	wrapper(_read(ptr + bfsize, buf_ptr_read, sizeof(buf_ptr_read)), 0);
+}
+
+void read_case_1() {
+	int page_number = 3;
+	size_t page_size = 10;
+	VA ptr = NULL;
+	char* buf_ptr_write = "aysd";
+	char* buf_ptr_read = (char*)malloc(sizeof(buf_ptr_write));
+	size_t bfsize = 5;
+	puts("Read with wrong parameters");
+	_init(page_number, page_size);
+	_malloc(&ptr, bfsize);
+	_write(ptr + sizeof(buf_ptr_write), buf_ptr_write, sizeof(buf_ptr_write));
+	wrapper(_read(ptr, buf_ptr_read, sizeof(buf_ptr_write)), -1);
+}
+
+void read_case_2() {
+	int page_number = 3;
+	size_t page_size = 10;
+	VA ptr;
+	char* buf_ptr_write = "aysd";
+	char* buf_ptr_read = (char*)malloc(sizeof(buf_ptr_write));
+	size_t bfsize = 5;
+	puts("Read with memory exit");
+	_init(page_number, page_size);
+	_malloc(&ptr, bfsize);
+	_write(ptr + sizeof(buf_ptr_write), buf_ptr_write, sizeof(buf_ptr_write));
+	wrapper(_read(ptr, buf_ptr_read, sizeof(buf_ptr_write)), -2);
+}
+
+void write_case0() {
+	int page_number = 3;
+	size_t page_size = 10;
+	VA ptr;
+	char* buf_ptr_write = "aysd";
+	size_t bfsize = 5;
+	puts("Success write");
+	_init(page_number, page_size);
+	_malloc(&ptr, bfsize);
+	wrapper(_write(ptr + bfsize, buf_ptr_write, sizeof(buf_ptr_write)), 0);
+}
+
+void write_case_1() {
+	int page_number = 3;
+	size_t page_size = 10;
+	VA ptr = NULL;
+	char* buf_ptr_write = "aysd";
+	size_t bfsize = 5;
+	puts("Write with wrong parameters");
+	_init(page_number, page_size);
+	_malloc(&ptr, bfsize);
+	wrapper(_write(ptr, buf_ptr_write, sizeof(buf_ptr_write)), -1);
+}
+
+void write_case_2() {
+	int page_number = 3;
+	size_t page_size = 10;
+	VA ptr;
+	char* buf_ptr_write = "aysd";
+	size_t bfsize = 5;
+	puts("Write with memory exit");
+	_init(page_number, page_size);
+	_malloc(&ptr, bfsize);
+	wrapper(_write(ptr, buf_ptr_write, sizeof(buf_ptr_write)), -2);
+}
+
+void free_case0() {
+	int page_number = 3;
+	size_t page_size = 10;
+	VA ptr1, ptr2, ptr3;
+	size_t bfsize = 5;
+	puts("Success free");
+	_init(page_number, page_size);
 	_malloc(&ptr1, bfsize);
 	_malloc(&ptr2, bfsize);
 	_malloc(&ptr3, bfsize);
-	_write(ptr1, bf, bfsize);
-	_read(ptr1, bf, bfsize);
-	_free(&ptr2);
+	wrapper(_free(ptr1 + bfsize), 0);
+}
+
+void free_case_1() {
+	int page_number = 3;
+	size_t page_size = 10;
+	VA ptr = NULL;
+	size_t bfsize = 5;
+	puts("Free with wrong parameters");
+	_init(page_number, page_size);
+	_malloc(&ptr, bfsize);
+	wrapper(_free(ptr + bfsize), -1);
+}
+
+int main() {
+	init_case0();
+	init_case_1();
+	malloc_case0();
+	malloc_case_1();
+	malloc_case_2();
+	write_case0();
+	write_case_1();
+	write_case_2();
+	//read_case0();
+	read_case_1();
+	read_case_2();
+	free_case0();
+	free_case_1();
 	puts("\nPress any key to quit:");
 	getchar();
 	return 0;
--- os-1/Исходные файлы/mmemory.c
+++ os-1/Исходные файлы/mmemory.c
@@ -8,14 +8,6 @@
 LinkedList *memory_storage;
 
 
-char* wrapper(int code) {
-	if (code) return "wrong parameters";
-	if (!code) return "success";
-	if (code == -1) return "unknown error";
-	if (code == -2) return "memory leak";
-}
-
-
 int _init(int n, int szPage) {
 	if (n < 0 || szPage < 0) return -1;
 	memory_storage = (LinkedList*)malloc(sizeof(LinkedList));
@@ -29,30 +21,27 @@ int _init(int n, int szPage) {
 
 int _malloc(VA *ptr, size_t szBlock) {
 	if (memory_storage == NULL) return 1;
-	if (ptr == NULL) return -1;
+	if (*ptr == NULL) return -1;
 	Node *iter_node = memory_storage->head;
-	int i = 0;
 	while (iter_node != NULL) {
-		printf("\niteration %d", i);
 		if (iter_node->value->empty) {
 			if (iter_node->value->size == szBlock) {
 				*ptr = iter_node->value->va;
 				iter_node->value->empty = false;
 				return 0;
 			}
-			if (iter_node->value->size > szBlock) {
+			else if (iter_node->value->size > szBlock) {
 				*ptr = iter_node->value->va;
 				iter_node->value->size -= szBlock;
 				MemoryBlock *block = create_memory_block(iter_node->value->va+szBlock, szBlock);
+				block->empty = false;
 				insert(memory_storage, iter_node, iter_node->next, block);
 				return 0;
 			}
 		}
 		if (iter_node->next == NULL) return -2;
-		i++;
 		iter_node = iter_node->next;
 	}
-	return 1;
 }
 
 int _free(VA ptr) {
@@ -63,20 +52,23 @@ int _free(VA ptr) {
 		if (iter_node->value->va == ptr) {
 			if ((iter_node->prev != NULL) && (iter_node->prev->value->empty)) {
 				iter_node->prev->value->size += iter_node->value->size;
-				iter_node = iter_node->prev;
 				delete_node(memory_storage, iter_node);
+				iter_node = iter_node->prev;
 				free(ptr);
+				ptr = NULL;
 				return 0;
 			}
 			if ((iter_node->next != NULL)&&(iter_node->next->value->empty)) {
 				iter_node->value->size += iter_node->next->value->size;
 				delete_node(memory_storage, iter_node->next);
 				free(ptr);
+				ptr = NULL;
 				return 0;
 			}
 			if (!iter_node->value->empty) {
 				iter_node->value->empty = true;
 				free(ptr);
+				ptr = NULL;
 				return 0;
 			}
 		}
@@ -95,10 +87,9 @@ int _read(VA ptr, void* pBuffer, size_t szBuffer) {
 			memcpy(pBuffer, ptr, szBuffer);
 			return 0;
 		}
-		if (iter_node->next == NULL) return 1;
+		if (iter_node->next == NULL) return -2;
 		iter_node = iter_node->next;
 	}
-	return 1;
 }
 
 
@@ -111,8 +102,7 @@ int _write(VA ptr, void* pBuffer, size_t szBuffer) {
 			memcpy(ptr, pBuffer, szBuffer);
 			return 0;
 		}
-		if (iter_node->next == NULL) return 1;
+		if (iter_node->next == NULL) return -2;
 		iter_node = iter_node->next;
 	}
-	return 1;
 }
